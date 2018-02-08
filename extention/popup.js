2d90d36c5e71b66b9ebadb66f6cafe3b1b20106a
Noräs Salman <noras.salman@live.com>
2018-02-08 11:28:49 +0100

    added alot of new algorithm implmentations to the encodder

--- extention/popup.js
+++ extention/popup.js
@@ -485,6 +485,64 @@ function decode_ascii85(a) {
     }(e, c[l]), h.fromCharCode.apply(h, e);
 }
 
+var base36 = {
+    encode: function (str) {
+        return Array.prototype.map.call(str, function (c) {
+            return c.charCodeAt(0).toString(36);
+        }).join("");
+    },
+    decode: function (str) {
+        //assumes one character base36 strings have been zero padded by encodeAscii
+        var chunked = [];
+        for (var i = 0; i < str.length; i = i + 2) {
+            chunked[i] = String.fromCharCode(parseInt(str[i] + str[i + 1], 36));
+        }
+        return chunked.join("");
+    },
+    encodeAscii: function (str) {
+        return Array.prototype.map.call(str, function (c) {
+            var b36 = base36.encode(c, "");
+            if (b36.length === 1) {
+                b36 = "0" + b36;
+            }
+            return b36;
+        }).join("");
+    },
+    decodeAscii: function (str) {
+        //ignores special characters/seperators if they're included
+        return str.replace(/[a-z0-9]{2}/gi, function (s) {
+            return base36.decode(s);
+        })
+    }
+};
+
+//BASe36
+function base36_encode(raw){
+return base36.encodeAscii(raw);
+}
+
+function base36_decode(raw){
+return base36.decodeAscii(raw);
+}
+
+function base32_encode(raw){
+return base32.encode(raw);
+}
+
+function base32_encode(raw){
+return base32.decode(raw);
+}
+
+
+function rot47(x)
+{var s=[];for(var i=0;i<x.length;i++)
+{var j=x.charCodeAt(i);if((j>=33)&&(j<=126))
+{s[i]=String.fromCharCode(33+((j+ 14)%94));}
+else
+{s[i]=String.fromCharCode(j);}}
+  return s.join('');}
+
+
 
 // BACON
 
@@ -596,3 +654,105 @@ raw_decimal=raw_decimal.replaceAll("\n","");
         xhrfactor.send();
 return "called";
 }
+
+
+   var base32 = {
+                    a: "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567",
+                    pad: "=",
+                    encode: function (s) {
+                        var a = this.a;
+                        var pad = this.pad;
+                        var len = s.length;
+                        var o = "";
+                        var w, c, r=0, sh=0;
+                        for(i=0; i<len; i+=5) {
+                            // mask top 5 bits
+                            c = s.charCodeAt(i);
+                            w = 0xf8 & c;
+                            o += a.charAt(w>>3);
+                            r = 0x07 & c;
+                            sh = 2;
+
+                            if ((i+1)<len) {
+                                c = s.charCodeAt(i+1);
+                                // mask top 2 bits
+                                w = 0xc0 & c;
+                                o += a.charAt((r<<2) + (w>>6));
+                                o += a.charAt( (0x3e & c) >> 1 );
+                                r = c & 0x01;
+                                sh = 4;
+                            }
+
+                            if ((i+2)<len) {
+                                c = s.charCodeAt(i+2);
+                                // mask top 4 bits
+                                w = 0xf0 & c;
+                                o += a.charAt((r<<4) + (w>>4));
+                                r = 0x0f & c;
+                                sh = 1;
+                            }
+
+                            if ((i+3)<len) {
+                                c = s.charCodeAt(i+3);
+                                // mask top 1 bit
+                                w = 0x80 & c;
+                                o += a.charAt((r<<1) + (w>>7));
+                                o += a.charAt((0x7c & c) >> 2);
+                                r = 0x03 & c;
+                                sh = 3;
+                            }
+
+                            if ((i+4)<len) {
+                                c = s.charCodeAt(i+4);
+                                // mask top 3 bits
+                                w = 0xe0 & c;
+                                o += a.charAt((r<<3) + (w>>5));
+                                o += a.charAt(0x1f & c);
+                                r = 0;
+                                sh = 0;
+                            }
+                        }
+                        // Calculate length of pad by getting the
+                        // number of words to reach an 8th octet.
+                        if (r!=0) { o += a.charAt(r<<sh); }
+                        var padlen = 8 - (o.length % 8);
+                        // modulus
+                        if (padlen==8) { return o; }
+                        if (padlen==1) { return o + pad; }
+                        if (padlen==3) { return o + pad + pad + pad; }
+                        if (padlen==4) { return o + pad + pad + pad + pad; }
+                        if (padlen==6) { return o + pad + pad + pad + pad + pad + pad; }
+                        console.log('there was some kind of error');
+                        console.log('padlen:'+padlen+' ,r:'+r+' ,sh:'+sh+', w:'+w);
+                    },
+                    decode: function(s) {
+                        var len = s.length;
+                        var apad = this.a + this.pad;
+                        var v,x,r=0,bits=0,c,o='';
+
+                        s = s.toUpperCase();
+
+                        for(i=0;i<len;i+=1) {
+                            v = apad.indexOf(s.charAt(i));
+                            if (v>=0 && v<32) {
+                                x = (x << 5) | v;
+                                bits += 5;
+                                if (bits >= 8) {
+                                    c = (x >> (bits - 8)) & 0xff;
+                                    o = o + String.fromCharCode(c);
+                                    bits -= 8;
+                                }
+                            }
+                        }
+                        // remaining bits are < 8
+                        if (bits>0) {
+                            c = ((x << (8 - bits)) & 0xff) >> (8 - bits);
+                            // Don't append a null terminator.
+                            // See the comment at the top about why this sucks.
+                            if (c!==0) {
+                                o = o + String.fromCharCode(c);
+                            }
+                        }
+                        return o;
+                    }
+                }
